597ec4b1c7b79a203284d106d3b82f885cd5163a
Andre Butarbutar <75428071+drebutbut@users.noreply.github.com>
2022-06-20 06:19:17 +0800

    Add files via upload

---
+++ Manager/testQRCODE.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="techstyle2.css">
+</head>
+
+<body>
+    <section class="header-in">
+	<header>
+	<img src="https://www.ump.edu.my/download/logo-ump-jawi-2021.png" alt="ump logo" width="150" height="80" align="left">
+		<h2>FK BOOKING UMP SYSTEM</h2>
+		<h3>Manager Login</h3>
+	</header>
+	</section>
+    <section class="form-bubble1" style="margin-top:100px;">
+        <!-- <div class="container"> -->
+            <h1>Welcome to UMP Booking</h1><br>
+            <center><table id=table1>
+            <tr>
+            <td><b>Manager</b><p>Click to Enter Manager Homepage:<br><br>
+                <a href="index.php" class="btn btn-info btn-lg">
+                    <span class="glyphicon glyphicon-qrcode"></span> Qrcode
+                </a>
+            </p></td>
+</tr>
+</table></center>
+        <!-- </div> -->
+    </section>
+    <footer><p style="font-size:20px">Faculty of Computing</p></footer>
+</body>
+
+</html>
